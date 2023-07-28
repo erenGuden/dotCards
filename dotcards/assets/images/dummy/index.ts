0cf2303b911af43f4769c0427eeaa41c3718065f
erenGuden <105981672+erenGuden@users.noreply.github.com>
2023-07-27 21:46:50 -0700

    folder structure

---
+++ dotcards/assets/images/dummy/index.ts
@@ -0,0 +1,6 @@
+export { default as image1 } from "./image1.png";
+export { default as image2 } from "./image2.png";
+export { default as image3 } from "./image3.png";
+export { default as image4 } from "./image4.png";
+
+export { default as map } from "./map.jpg";
---
+++ dotcards/assets/images/index.js
@@ -0,0 +1,8 @@
+const minus = require("./minus.svg");
+const plus = require("./plus.svg");
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  minus,
+  plus,
+};
